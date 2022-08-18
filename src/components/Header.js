7130d718bff41031e722dfa476b0e0cd0fb931ac
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2022-08-18 17:20:23 +0600

    header mobile complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useRef } from 'react'
 
 const Header = () => {
+  const hamburger = useRef(null)
+  const navlist = useRef(null)
+
+  const navToggle = () => {
+    hamburger.current.classList.toggle('open')
+    navlist.current.classList.toggle('open')
+  }
 	return (
 		<header className='header-bg'>
 			<div className='header container'>
@@ -8,18 +16,25 @@ const Header = () => {
 					<img src='/logo.png' />
 				</div>
 				<nav className='navbar'>
-					<h3 className='active'>Dashboard</h3>
-					<h3>Camapign</h3>
-					<h3>Hypersocial</h3>
-					<h3>Insights</h3>
+          <div ref={navlist} className='navlist'>
+            <h3 className='active'>Dashboard</h3>
+            <h3>Camapign</h3>
+            <h3>Hypersocial</h3>
+            <h3>Insights</h3>
+          </div>
+					<button className='hamburger' ref={hamburger} onClick={navToggle} >
+						<span className='hamburger-top'></span>
+						<span className='hamburger-middle'></span>
+						<span className='hamburger-bottom'></span>
+					</button>
 				</nav>
 				<div className='profile'>
 					<img src='/bell.png' />
 					<img src='/moon.png' />
 					<h4>
-            <span>Hi,</span>
-            <span className='username'>Rakim</span>
-          </h4>
+						<span>Hi,</span>
+						<span className='username'>Rakib</span>
+					</h4>
 					<img src='/profile-icon.png' />
 				</div>
 			</div>
