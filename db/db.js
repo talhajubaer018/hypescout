cf88e0c75a6562265408696da47a25867cadf565
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2022-08-18 20:43:37 +0600

    profile content added

---
+++ db/db.js
@@ -0,0 +1,88 @@
+const profileData = [
+  {
+    id: 1,
+    image: '/maksudul.png',
+    name: 'Maksudul Hasan',
+    location: 'Dhaka Bangladesh',
+    joinDate: 'July 26th, 2022',
+    fbLink: '',
+    instagramLink: '',
+    linkedinLink: '',
+    twitterLink: '',
+    followers: '12k',
+    category: 'Fashion',
+    gender: 'Male'
+  },
+  {
+    id: 2,
+    image: '/refez.png',
+    name: 'Refez Hossain',
+    location: 'Barishal Bangladesh',
+    joinDate: 'July 22th, 2022',
+    fbLink: '',
+    instagramLink: '',
+    linkedinLink: '',
+    twitterLink: '',
+    followers: '15k',
+    category: 'Sports',
+    gender: 'Male'
+  },
+  {
+    id: 3,
+    image: '/john-doe.png',
+    name: 'John Doe',
+    location: 'Dhaka Bangladesh',
+    joinDate: 'July 26th, 2022',
+    fbLink: '',
+    instagramLink: '',
+    linkedinLink: '',
+    twitterLink: '',
+    followers: '5k',
+    category: 'Entertainment',
+    gender: 'Male'
+  },
+  {
+    id: 4,
+    image: '/neymar.png',
+    name: 'Neymar Jr.',
+    location: 'Dhaka Bangladesh',
+    joinDate: 'June 26th, 2022',
+    fbLink: '',
+    instagramLink: '',
+    linkedinLink: '',
+    twitterLink: '',
+    followers: '33k',
+    category: 'Football',
+    gender: 'Male'
+  },
+  {
+    id: 5,
+    image: '/jonathan.png',
+    name: 'Jonathan Smith',
+    location: 'Dhaka Bangladesh',
+    joinDate: 'April 20th, 2022',
+    fbLink: '',
+    instagramLink: '',
+    linkedinLink: '',
+    twitterLink: '',
+    followers: '2k',
+    category: 'Photography',
+    gender: 'Male'
+  },
+  {
+    id: 6,
+    image: '/john-sina.png',
+    name: 'John Sina',
+    location: 'Dhaka Bangladesh',
+    joinDate: 'January 15th, 2022',
+    fbLink: '',
+    instagramLink: '',
+    linkedinLink: '',
+    twitterLink: '',
+    followers: '50k',
+    category: 'Wrestling',
+    gender: 'Male'
+  },
+]
+
+export default profileData
--- src/components/ProfileContent.js
+++ src/components/ProfileContent.js
@@ -1,37 +1,40 @@
 import React from 'react'
+import profileData from '../../db/db.js'
 
 const ProfileContent = () => {
 	return (
 		<section className='profile-section container'>
-			<div className='profile-card'>
-				<img src='/maksudul.png' alt='maksudul' />
-				<h1 className='profile-name'>Maksudul Hasan</h1>
-				<div className='location'>
-					<img src='/map-icon.png' alt='map' />
-					<h2>Dhaka Bangladesh</h2>
+			{profileData.map((data) => (
+				<div className='profile-card' key={data.id}>
+					<img src={data.image} alt='maksudul' />
+					<h1 className='profile-name'>{data.name}</h1>
+					<div className='location'>
+						<img src='/map-icon.png' alt='map' />
+						<h2>{data.location}</h2>
+					</div>
+					<div className='time'>
+						<img src='/clock-icon.png' alt='clock' />
+						<h2>{data.joinDate}</h2>
+					</div>
+					<div className='social-icons'>
+						<img src='/fb.png' alt='facebook' />
+						<img src='/instagram.png' alt='instagram' />
+						<img src='/linkedin.png' alt='linkedin' />
+						<img src='/twitter.png' alt='twitter' />
+					</div>
+					<div className='profile-info'>
+						<h3>
+							<span>{data.followers}</span> <br /> Followers
+						</h3>
+						<h3>
+							<span>{data.category}</span> <br /> Categories
+						</h3>
+						<h3>
+							<span>{data.gender}</span> <br /> Gender
+						</h3>
+					</div>
 				</div>
-				<div className='time'>
-					<img src='/clock-icon.png' alt='clock' />
-					<h2>Joined On: July 26th, 2022</h2>
-				</div>
-				<div className='social-icons'>
-					<img src='/fb.png' alt='facebook' />
-					<img src='/instagram.png' alt='instagram' />
-					<img src='/linkedin.png' alt='linkedin' />
-					<img src='/twitter.png' alt='twitter' />
-				</div>
-				<div className='profile-info'>
-					<h3>
-						<span>12k</span> <br /> Followers
-					</h3>
-					<h3>
-						<span>Fashion</span> <br /> Categories
-					</h3>
-					<h3>
-						<span>Male</span> <br /> Gender
-					</h3>
-				</div>
-			</div>
+			))}
 		</section>
 	)
 }
