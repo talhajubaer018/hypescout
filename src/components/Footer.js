080da73fc6090a1560fdf0d49ad7bd8ad3956722
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2022-08-19 00:54:25 +0600

    footer mobile fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,18 +5,32 @@ const Footer = () => {
 	return (
 		<footer className='footer-bg'>
 			<div className='footer container'>
-				<div>
+				<div className='footer-info'>
 					<img className='footer-logo' src='/logo.png' alt='logo' />
 					<p className='footer-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum sed ultricies volutpat rhoncus faucibus sit. </p>
 					<Socials />
 				</div>
 				<div className='footer-company'>
-					<h2>Company</h2>
-					<h3>About Us</h3>
-					<h3>Careers</h3>
-					<h3>Case Study</h3>
-					<h3>Blog</h3>
-					<h3>Contact Us</h3>
+          <div>
+            <h2>Company</h2>
+            <h3>About Us</h3>
+            <h3>Careers</h3>
+            <h3>Case Study</h3>
+            <h3>Blog</h3>
+            <h3>Contact Us</h3>
+          </div>
+					<div>
+						<div className='influencer-mobile'>
+							<h2>Influencer</h2>
+							<h3>Join as Influencer</h3>
+							<h3>HypeSocial</h3>
+						</div>
+						<div className='advertise-mobile'>
+							<h2>Advertiser</h2>
+							<h3>Join as Advertiser</h3>
+							<h3>Hypelink</h3>
+						</div>
+					</div>
 				</div>
 				<div className='influencer'>
 					<h2>Influencer</h2>
