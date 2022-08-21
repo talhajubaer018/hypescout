0b585b254981af2cb1e3653d03c55c1789758680
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2022-08-21 15:01:42 +0600

    modal mobile fix

--- src/components/SearchCard.js
+++ src/components/SearchCard.js
@@ -5,32 +5,34 @@ import FilterModal from './FilterModal.js'
 
 const SearchCard = ({ search }) => {
 	const modal = useRef(null)
+	const modalBg = useRef(null)
 
 	const modalOpen = () => {
 		modal.current.classList.toggle('show')
+		modalBg.current.classList.toggle('show')
 	}
 	const modalClose = () => {
-		modal.current.classList.remove('show')
+		modalBg.current.classList.remove('show')
 	}
 	return (
 		<section className='search-card-bg'>
 			<div className='search-card container'>
-				<h1 className='card-header'>Profile(6)</h1>
+				<h1 className='card-header'>Profile({profileData.length})</h1>
 				<div className='searchbox'>
 					<input type='text' placeholder='Search Profile' onChange={(e) => search(e.target.value)} />
 					<div className='search-icon'>
 						<img src='/search.png' alt='search' />
 					</div>
 				</div>
-				<div className='modal-bg'></div>
+				<div ref={modalBg} className='modal-bg'></div>
 				<div className='filter-button' onClick={modalOpen}>
 					<img src='/filter-icon.png' alt='filter' />
 					<div>
 						<h3>Advanced Filter</h3>
 					</div>
-					<div ref={modal} className='modal-container'>
-						<FilterModal profileData={profileData} />
-					</div>
+				</div>
+				<div ref={modal} className='modal-container'>
+					<FilterModal profileData={profileData} />
 				</div>
 			</div>
 		</section>
